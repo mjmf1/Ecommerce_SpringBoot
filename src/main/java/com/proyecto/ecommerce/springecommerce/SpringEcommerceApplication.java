e905f4c05eaf565729599db8354be31d68d8c2f4
mjmf1 <marcomora61@gmail.com>
2024-02-12 22:37:50 -0500

    Integrar Boostrap y la home del administrador

--- src/main/java/com/proyecto/ecommerce/springecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/SpringEcommerceApplication.java
@@ -3,13 +3,11 @@ package com.proyecto.ecommerce.springecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.sql.DataSource;
-
 @SpringBootApplication
 public class SpringEcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEcommerceApplication.class, args);
 	}
-
 }
+
