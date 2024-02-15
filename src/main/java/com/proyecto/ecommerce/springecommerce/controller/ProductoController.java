1ae91c401acc6c984621cc807b51ff9633bd8411
mjmf1 <marcomora61@gmail.com>
2024-02-15 10:26:06 -0500

    Implementar el método guardar producto

--- src/main/java/com/proyecto/ecommerce/springecommerce/controller/ProductoController.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controller/ProductoController.java
@@ -1,8 +1,11 @@
 package com.proyecto.ecommerce.springecommerce.controller;
 
 import com.proyecto.ecommerce.springecommerce.model.producto;
+import com.proyecto.ecommerce.springecommerce.model.usuario;
+import com.proyecto.ecommerce.springecommerce.service.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductoController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
+    @Autowired
+    private ProductoService productoService;
 
     @GetMapping("")
     public String show() {
@@ -28,5 +34,9 @@ public class ProductoController {
     @PostMapping("/save")
     public String save(producto producto) {
         LOGGER.info("Este es el objeto producto {}", producto);
+        usuario u;
+        u = new usuario(1,"","","","","","","");
+        producto.setUsuario(u);
+        productoService.save(producto);
         return "redirect:/productos";
     }}
