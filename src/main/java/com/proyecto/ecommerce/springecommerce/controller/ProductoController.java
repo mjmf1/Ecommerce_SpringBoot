dcb753e674734cc7fb628436c71869122a78f5e4
mjmf1 <marcomora61@gmail.com>
2024-02-16 15:30:30 -0500

    Crear la funcionalidad para editar un registro producto

--- src/main/java/com/proyecto/ecommerce/springecommerce/controller/ProductoController.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controller/ProductoController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 
 @Controller
@@ -41,4 +44,22 @@ public class ProductoController {
         producto.setUsuario(u);
         productoService.save(producto);
         return "redirect:/productos";
-    }}
+    }
+    @GetMapping("/edit/{id}")
+    public String edit( @PathVariable Integer id, Model model){
+        producto    producto = new producto();
+        Optional<producto> optionalProducto = productoService.get(id);
+        producto = optionalProducto.get();
+
+        LOGGER.info("Producto buscado: {}", producto);
+        model.addAttribute("producto", producto);
+
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public  String update(producto producto) {
+        productoService.update(producto);
+        return "redirect:/productos";
+    }
+}
