2e4d23d099aa55583b500a4886bf7d56619bf0ef
mjmf1 <marcomora61@gmail.com>
2024-02-27 18:27:58 -0500

    Mostrar el carrito desde la opción carrito del menú y agregar validacion para que no se pueda elegir 2 veces el mismo producto.

--- src/main/java/com/proyecto/ecommerce/springecommerce/controller/homeController.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controller/homeController.java
@@ -61,7 +61,15 @@ public class homeController {
         detalleOrden.setTotal(producto.getPrecio() * cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //Validar que el producto no se añada 2 veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p->p.getProducto().getId() == idProducto);
+
+        if (!ingresado){
+            detalles.add(detalleOrden);
+        }
+
+
 
         sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 
@@ -94,7 +102,12 @@ public class homeController {
 
 
         return "usuario/carrito";
-
-
     }
+
+    @GetMapping("/getCart")
+    public String getCart( Model model) {
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+        return "usuario/carrito";    }
 }
