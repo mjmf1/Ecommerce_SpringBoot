46758b8f88ba4ad48c6a083644e3987a5be002c7
mjmf1 <marcomora61@gmail.com>
2024-02-27 17:20:14 -0500

    Funcionalidad eliminar productos del carrito

--- src/main/java/com/proyecto/ecommerce/springecommerce/controller/homeController.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controller/homeController.java
@@ -71,4 +71,30 @@ public class homeController {
 
         return "usuario/carrito";
     }
+
+    // Quitar productos del carrito
+
+    @GetMapping("/delete/cart/{id}")
+    public String deleteProductoCart(@PathVariable Integer id, Model model ){
+        //Lista nueva de productos
+        List<DetalleOrden> ordenesNuevas = new ArrayList<DetalleOrden>();
+        for (DetalleOrden detalleOrden : detalles){
+            if (detalleOrden.getProducto().getId()!=id){
+                ordenesNuevas.add(detalleOrden);
+            }
+        }
+        //Poner la nueva lista con los productos restantes
+        detalles = ordenesNuevas;
+        double sumaTotal = 0;
+        sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
+
+
+        return "usuario/carrito";
+
+
+    }
 }
