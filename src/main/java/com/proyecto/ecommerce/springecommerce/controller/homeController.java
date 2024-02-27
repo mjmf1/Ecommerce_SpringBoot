2726d038a3972afca4d43461703032caa60dc5bc
mjmf1 <marcomora61@gmail.com>
2024-02-27 13:51:22 -0500

    Mostrar los datos del producto en otra vista

--- src/main/java/com/proyecto/ecommerce/springecommerce/controller/homeController.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controller/homeController.java
@@ -1,15 +1,20 @@
 package com.proyecto.ecommerce.springecommerce.controller;
 
 import com.proyecto.ecommerce.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class homeController {
+
+    private  final Logger log = LoggerFactory.getLogger(homeController.class);
     @Autowired
     private ProductoService productoService;
     @GetMapping("")
@@ -17,5 +22,10 @@ public class homeController {
         model.addAttribute("productos", productoService.findAll());
 
         return "usuario/home";
+    }
+@GetMapping("productohome/{id}")
+    public String productoHome( @PathVariable Integer id){
+        log.info("Id enviado como parametro {}", id);
+        return  "usuario/productohome";
     }
 }
