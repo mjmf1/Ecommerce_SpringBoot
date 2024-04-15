4da59f593de4058ca5a9b9157fb58fc6151aa6f0
mjmf1 <marcomora61@gmail.com>
2024-04-15 12:26:40 -0500

    Funcionalidad para ver los detalles de una orden

--- src/main/java/com/proyecto/ecommerce/springecommerce/controller/AdminController.java
+++ src/main/java/com/proyecto/ecommerce/springecommerce/controller/AdminController.java
@@ -1,17 +1,22 @@
 package com.proyecto.ecommerce.springecommerce.controller;
 
+import com.proyecto.ecommerce.springecommerce.model.Orden;
 import com.proyecto.ecommerce.springecommerce.service.IOrdenService;
 import com.proyecto.ecommerce.springecommerce.service.IUsuarioService;
 import com.proyecto.ecommerce.springecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.proyecto.ecommerce.springecommerce.model.producto;
-import com.proyecto.ecommerce.springecommerce.service.ProductoService;
+
 
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/admin")
@@ -23,6 +28,8 @@ public class AdminController {
     private IUsuarioService usuarioService;
     @Autowired
     private IOrdenService ordenService;
+
+    private Logger logg = LoggerFactory.getLogger(AdminController.class);
 
     @GetMapping("")
     public String home( Model model) {
@@ -40,6 +47,13 @@ public class AdminController {
     public String ordenes(Model model) {
         model.addAttribute("ordenes", ordenService.findAll());
         return "admin/ordenes";
+    }
+    @GetMapping("/detalle/{id}")
+    public String detalle(Model model, @PathVariable Integer id) {
+        logg.info("Id de la orden {}", id );
+        Orden orden =ordenService.findById(id).get();
+        model.addAttribute("detalles", orden.getDetalle());
+        return "admin/detalleOrden";
     }
 }
 
